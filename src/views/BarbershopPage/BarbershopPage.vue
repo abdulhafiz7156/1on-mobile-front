f5b04e70b864bd5c4cf8bbcd31e582b319fd5190
abdulhafiz7156 <xxafiz69@gmail.com>
2024-05-03 12:22:25 +0500

    added header burger menu sidebar page

--- src/views/BarbershopPage/BarbershopPage.vue
+++ src/views/BarbershopPage/BarbershopPage.vue
@@ -11,25 +11,38 @@
   <div class="container">
     <div class="staff">
       <h4>Hodimlar</h4>
-      <StaffCard @click="visibleTop = true" />
-      <StaffCard @click="visibleTop = true" />
-      <StaffCard @click="visibleTop = true" />
+      <StaffCard @click="visibleTop = true"/>
+      <StaffCard @click="visibleTop = true"/>
+      <StaffCard @click="visibleTop = true"/>
       <p v-if="allStaff" class="all__staff">+ Barcha hodimlar</p>
     </div>
     <div class="services">
       <h4>Asosiy xizmatlar</h4>
-      <card />
-      <card />
-      <card />
-      <card />
+      <card/>
+      <card/>
+      <card/>
+      <card/>
     </div>
   </div>
   <NavigationBar></NavigationBar>
   <div>
     <div class="card flex justify-content-center">
-      <Sidebar v-model:visible="visibleTop" position="bottom" style="height: 90%;background: #181C20; border-top-left-radius: 28px;  border-top-right-radius: 28px; padding: 20px;"  class="sidebar">
-        <StaffCard :plusVisible="plusVisible" />
-        <Button>Tasdiqlash</Button>
+      <Sidebar v-model:visible="visibleTop" position="bottom"
+               class="sidebar"
+               close-icon="pi pi-minus"
+      >
+        <StaffCard :plusVisible="plusVisible"/>
+        <div class="br-services custom__services" data-v-eb7e32a3="">
+          <h3 data-v-eb7e32a3="">Xizmatlar</h3>
+          <img src="/src/assets/icons/arrow-right-icon.svg" data-v-eb7e32a3="">
+          <p data-v-eb7e32a3="">Soqol olish, Soch turmagi</p>
+        </div>
+        <div class="br-services custom__services" data-v-eb7e32a3="">
+          <h3 data-v-eb7e32a3="">Kun va vaqti</h3>
+          <img src="/src/assets/icons/arrow-right-icon.svg" data-v-eb7e32a3="">
+          <p data-v-eb7e32a3="">Soqol olish, Soch turmagi</p>
+        </div>
+          <Button>Tasdiqlash</Button>
       </Sidebar>
     </div>
   </div>
@@ -47,9 +60,9 @@ import Button from "../../components/Button/Button.vue";
 export default {
   components: {card, StaffCard, NavigationBar, Header, Button},
   setup() {
-    const headerTitle= ref("Topor barbershop")
+    const headerTitle = ref("Topor barbershop")
     const allStaff = ref(false)
-    const visibleTop = ref(true)
+    const visibleTop = ref(false)
     const plusVisible = ref(false)
     return {
       headerTitle,
@@ -64,6 +77,14 @@ export default {
 <style scoped>
 .header {
   padding: 30px 4px 0px 4px;
+}
 
+.custom__services {
+  background: #101418 !important;
+}
+
+
+.services #card:last-child {
+  margin-bottom: 100px;
 }
 </style>
