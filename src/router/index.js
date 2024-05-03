54715d8d844a8dded567053df7952310b934e8a9
Muhammadrizo <mtursunboev13@gmail.com>
2024-05-03 17:03:30 +0500

    merged abdulhafiz and dev branches

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home/Home.vue';
+import BarberProfile from '../views/barber/BarberProfile.vue';
+import ServiceCard from '../views/Services/ServiceCard.vue';
+import ServiceCardWithoutImage from '../views/ServiceCardWithoutImage/ServiceCardWithoutImage.vue';
 import BarbershopPage from "../views/BarbershopPage/BarbershopPage.vue";
 import OrderDate from "../views/OrderDate/OrderDate.vue";
 import Employee from "../views/Employee/Employee.vue";
 import Order from "../views/Order/Order.vue";
-import BarberProfile from '../views/barber/BarberProfile.vue';
-import ServiceCard from '../components/ServiceCard/card.vue';
-import ServiceCardWithoutImage from '../components/ServiceCardWithoutImage/CardWithoutImage.vue';
-import Auth from '../views/Auth/Auth.vue';
+import Auth from "../views/Auth/Auth.vue";
+
 
 const router = createRouter({
   history: createWebHistory(),
